2c76814b0b289f4dce6fc00a056c972190480000
Hari Seldon <webseiten.designer@googlemail.com>
2013-05-25 18:59:25 +0200

    * Implemented parser * Tuned lexer to accept \" escapes * Added several tests for the lexer

--- core/wrightscript/parser.py
+++ core/wrightscript/parser.py
@@ -1,12 +1,203 @@
 '''
 The parser takes a string (presumely from a file) containing Wrightscript
-and outputs an abstract syntax tree (AST) of statements.
+and returns an abstract syntax tree (AST) of statements. It only performs
+the syntactic analysis, so don't expect functions to be extracted or label
+identifiers to be bound to positions in the StatementSequence. 
 '''
 from ..include.ply import yacc as yacc
+from lexer import tokens
 from AST import *
 
-# TODO make rules
+##
+# RULES
+#
+# As PLY does only track line numbers of tokens, we return tuples of (node, lineno)
+# for every statement we require a line number of and extract this information
+# when adding the statement to the StatementSequence.
 
-# note: parse implicit text box as Function('wrightscript', [content])
+def p_start_emptyspace(p):
+    'start : NEWLINE statements'
+    p[0] = p[2]
+    
+def p_start(p):
+    'start : statements'
+    p[0] = p[1]
 
-parser = yacc.yacc()
+def p_statements_multiple(p):
+    'statements : statements NEWLINE optspace statement'
+    (node, lineno) = p[4]
+    p[0] = p[1] # reuse statement sequence
+    p[0].add(node, lineno)
+
+def p_statements_single(p):
+    'statements : statement'
+    ss = StatementSequence()
+    (node, lineno) = p[1]
+    ss.add(node, lineno)
+    p[0] = ss
+
+def p_statement_expression(p):
+    'statement : expression'
+    p[0] = p[1]
+
+def p_statement_label(p):
+    'statement : LABEL IDENTIFIER'
+    p[0] = (Label(Identifier(p[2])), p.lineno(1))
+
+def p_expression_funcall(p):
+    'expression : IDENTIFIER args'
+    p[0] = (Call(Identifier(p[1]), p[2]), p.lineno(1))
+    
+def p_expression_implicittb(p):
+    'expression : STRING'
+    p[0] = (Call(Identifier('textbox'), [String(p[1])]), p.lineno(1))
+    
+def p_statement_resume(p):
+    'statement : RESUME'
+    p[0] = (Resume(), p.lineno(1))
+
+def p_statement_goto(p):
+    'statement : GOTO IDENTIFIER'
+    p[0] = (Goto(Identifier(p[2])), p.lineno(1))
+    
+def p_expression_conditional(p):
+    'statement : IS arg thenexpr elseexpr'
+    p[0] = (Is(p[2], p[3], p[4]), p.lineno(1))
+    
+def p_expression_exit(p):
+    'statement : EXIT'
+    p[0] = (Exit(), p.lineno(1))
+    
+def p_statement_fundef(p):
+    'statement : DEF IDENTIFIER params NEWLINE expressions NEWLINE ENDDEF'
+    p[0] = (Function(Identifier(p[2]), p[3], p[5]), p.lineno(1))
+
+def p_params_empty(p):
+    'params : empty'
+    p[0] = list()
+    
+def p_params(p):
+    'params : params IDENTIFIER'
+    paramlist = p[1]
+    paramlist.append(Identifier(p[2]))
+    p[0] = paramlist
+
+def p_expressions_single(p):
+    'expressions : optspace expression optspace'
+    ss = StatementSequence()
+    (node, lineno) = p[1]
+    ss.add(node, lineno)
+    p[0] = ss
+
+def p_expressions_multiple(p):
+    'expressions : expressions NEWLINE optspace expression'
+    (node, lineno) = p[3]
+    p[0] = p[1] # reuse statement sequence
+    p[0].add(node, lineno)
+    
+def p_expressions_return_empty(p):
+    'expressions : expressions NEWLINE RETURN'
+    p[0] = (Return(None), p.lineno(2))
+    
+def p_expressions_return_value(p):
+    'expressions : expressions NEWLINE RETURN arg'
+    p[0] = (Return(p[4]), p.lineno(2))
+    
+
+def p_thenexpr(p):
+    '''thenexpr : GOTO IDENTIFIER
+                | funcall'''
+    if p[1] == 'goto':
+        p[0] = Goto(Identifier(p[2]))
+    else:
+        p[0] = p[1]
+
+def p_elseexpr(p):
+    '''elseexpr : ELSE thenexpr
+                | empty'''
+    if p[1] == 'else':
+        p[0] = p[2]
+    else:
+        p[0] = None
+
+
+# Doubled for efficient lineno counting of statements; see PLY documentation for details.
+def p_funcall(p):
+    'funcall : IDENTIFIER args'
+    p[0] = Call(Identifier(p[1]), p[2])
+    
+def p_args_empty(p):
+    'args : empty'
+    p[0] = list()
+
+def p_args_arg(p):
+    'args : args arg'
+    arglist = p[1]
+    arglist.append(p[2])
+    p[0] = arglist
+    
+def p_arg_identifier(p):
+    'arg : IDENTIFIER'
+    p[0] = Identifier(p[1])
+    
+def p_arg_string(p):
+    'arg : STRING'
+    p[0] = String(p[1])
+
+def p_arg_number(p):
+    'arg : NUMBER'
+    p[0] = Number(p[1])
+
+def p_arg_boolean(p):
+    'arg : BOOLEAN'
+    p[0] = Boolean(p[1])
+
+def p_arg_funcall(p):
+    'arg : LPAREN funcall RPAREN'
+    p[0] = p[2]
+
+def p_optspace(p):
+    '''optspace : empty
+                | NEWLINE'''
+    pass
+
+def p_empty(p):
+    'empty :'
+    pass
+
+##
+# Error handling.
+
+class SyntaxError():
+    def __init__(self, token):
+        self._token = token
+        
+    def token(self):
+        return self._token
+    
+    def __repr__(self):
+        return 'Syntax Error: Unexpected ' + repr(self._token.type) + ' at line ' + repr(self._token.lineno)
+
+errors = list()
+
+def p_error(t):
+    errors.append(SyntaxError(t))
+    # Try reaching the next statement (see PLY documentation for yacc)
+    while 1:
+        tok = yacc.token()
+        if not tok or tok.type == 'NEWLINE': break
+    yacc.restart()
+    
+# Build the parser
+_parser = yacc.yacc()
+
+def resetErrors():
+    errors = list()
+
+def parse(script):
+    '''String => StatementSequence
+    
+    Parses the given script and returns the
+    resulting abstract syntax tree.'''
+    resetErrors()
+    return _parser.parse(script)
---
+++ test/core/wrightscript/TestLexer.py
@@ -0,0 +1,143 @@
+'''
+Tests the lexer.
+'''
+import unittest
+from core.wrightscript.lexer import lexer, ParseException
+
+class Test(unittest.TestCase):
+
+    def parseAsTypeList(self, string):
+        lexer.input(string)
+        return [token.type for token in lexer]
+    
+    def parseAsValueList(self, string):
+        lexer.input(string)
+        return [token.value for token in lexer]
+
+    def testReservedWords(self):
+        """Tests that reserved words are recognized and all other words are identifiers."""
+        input = '''
+        label
+        resume
+        is else
+        goto
+        exit
+        def
+        enddef
+        return
+        exits
+        labeled
+        '''
+        self.assertEqual(self.parseAsTypeList(input), [
+                     'NEWLINE',
+                     'LABEL',
+                     'NEWLINE',
+                     'RESUME',
+                     'NEWLINE',
+                     'IS',
+                     'ELSE',
+                     'NEWLINE',
+                     'GOTO',
+                     'NEWLINE',
+                     'EXIT',
+                     'NEWLINE',
+                     'DEF',
+                     'NEWLINE',
+                     'ENDDEF',
+                     'NEWLINE',
+                     'RETURN',
+                     'NEWLINE',
+                     'IDENTIFIER',
+                     'NEWLINE',
+                     'IDENTIFIER',
+                     'NEWLINE'])
+        
+    def testStrings(self):
+        """Tests that strings are parsed correctly."""
+        input = '\n"Something"'
+        self.assertEqual(self.parseAsTypeList(input), ['NEWLINE', 'STRING'])
+        input = '"Hello World!"'
+        self.assertEqual(self.parseAsValueList(input), ['Hello World!'])
+        input = '"Line\nBreak"'
+        self.assertEqual(self.parseAsValueList(input), ['Line\nBreak'])
+        input = '"Ignore return or label"'
+        self.assertEqual(self.parseAsValueList(input), ['Ignore return or label'])
+    
+    def testSubsequentStrings(self):
+        """Checks that the parser is no greedy with strings, so that
+        two strings following each other are parsed as two different strings."""
+        input = '"Hello" "World"'
+        self.assertEquals(self.parseAsTypeList(input), ['STRING', 'STRING'])
+        
+    def testStringEscape(self):
+        """Checks that string sequences can have escaped quotes."""
+        self.assertEquals(self.parseAsValueList(r'"What do you mean by \"accident\"?"'), ['What do you mean by "accident"?'])
+        pass
+    
+    def testNumbers(self):
+        """Tests that numbers are parsed correctly and converted to integers."""
+        input = '\n300'
+        self.assertEqual(self.parseAsTypeList(input), ['NEWLINE', 'NUMBER'])
+        input = '42 100'
+        self.assertEqual(self.parseAsValueList(input), [42, 100])
+        
+        try:
+            input = "-42"
+            self.parseAsTypeList(input)
+        except ParseException:
+            pass
+        else:
+            self.fail("Negative values are currently not supported and should raise a ParseException")
+        
+        try:
+            input = "42.0"
+            self.parseAsTypeList(input)
+        except ParseException:
+            pass
+        else:
+            self.fail("Floats are currently not supported and should raise a ParseException")
+    
+    def testBooleans(self):
+        """Tests that boolean literals are parsed correctly."""
+        input='true false'
+        self.assertEqual(self.parseAsTypeList(input), ['BOOLEAN', 'BOOLEAN'])
+        self.assertEqual(self.parseAsValueList(input), [True, False])
+        
+    def testInvalidIdentifier(self):
+        """An Identifier can't have a number as the first character."""
+        self.assertRaises(ParseException, self.parseAsTypeList, "42_invalid")
+        
+    def testParenthesis(self):
+        """A call to print with the result of a math expression."""
+        input = 'print (add 5 5)'
+        self.assertEqual(self.parseAsTypeList(input), ['IDENTIFIER', 'LPAREN', 'IDENTIFIER', 'NUMBER', 'NUMBER', 'RPAREN'])
+        
+    def testIgnoreComments(self):
+        """Checks that single line and multiline comments are ignored."""
+        input = '''
+        # This is a single line comment
+        foo
+        /* Multiline
+        Comment */
+        bar'''
+        self.assertEqual(self.parseAsTypeList(input), ['NEWLINE', 'NEWLINE', 'IDENTIFIER', 'NEWLINE', 'NEWLINE', 'IDENTIFIER'])
+
+    def testLineNumbers(self):
+        """Checks that line numbers are reset by the lexer for new input.
+        See Bug https://code.google.com/p/ply/issues/detail?id=20"""
+        lexer.input('a')
+        tokens = [token for token in lexer]
+        self.assertEquals(tokens[0].lineno, 1)
+        lexer.input('a')
+        tokens = [token for token in lexer]
+        self.assertEquals(tokens[0].lineno, 1)
+        
+    def testCommentNewline(self):
+        """Ensures that a comment with a newline leaves the newline intact."""
+        input = 'test #comment\n 5'
+        self.assertEqual(self.parseAsTypeList(input), ['IDENTIFIER', 'NEWLINE', 'NUMBER'])
+        
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
